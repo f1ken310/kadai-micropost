81ef719bfb95b09ee059842ef4cf1b16ea2ee718
takuma.sato <tkm310@gmail.com>
2021-01-24 14:10:13 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessons#new'
+  post 'login', to: 'sessons#create'
+  delete 'logout', to: 'sessons#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
